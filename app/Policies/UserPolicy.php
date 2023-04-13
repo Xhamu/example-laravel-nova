7ff31fb09e5e269922b6084307c4c49fb2b46195
Samuel <samuelgeminis5@gmail.com>
2023-04-13 10:31:49 +0200

    politica de usuario para rol 'editor' mejorado

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -20,7 +20,7 @@ class UserPolicy
      */
     public function view(User $user, User $model): bool
     {
-        return $user->id === $model->id || $user->hasRole('admin');
+        return $user->id === $model->id || $user->hasRole('admin') || $user->hasRole('editor');
     }
 
     /**
